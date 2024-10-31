0056d373c9ead5d67aeb24945cf1461b455c94f5
Pedro Bueno <pedrolbb.e@gmail.com>
2024-10-31 11:52:27 -0300

    refactor: main App

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,103 +1,27 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import {
-  Camera,
-  Templates,
-  runAsync,
-  useCameraDevice,
-  useCameraFormat,
-  useCameraPermission,
-  useFrameProcessor,
-} from 'react-native-vision-camera';
-import * as plugins from 'react-native-vision-camera-mlkit';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import React, { useRef } from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import type { Camera as RNVCCamera } from 'react-native-vision-camera';
+import { Camera } from './components/Camera';
 
 export default function App() {
-  const [targetFps] = useState(60);
-
-  const camera = useRef<Camera>(null);
-
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { textRecognizer } = plugins.useTextRecognizer({ language: 'LATIN' });
-
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { barcodeScanner } = plugins.useBarcodeScanner({
-    formats: ['QR_CODE', 'AZTEC'],
-    enableAllPotentialBarcodes: false,
-    invertColors: false,
-  });
-
-  // @ts-ignore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { imageLabeler } = plugins.useImageLabeler({
-    confidenceThreshold: 60,
-  });
-
-  const { objectDetector } = plugins.useObjectDetector({
-    enableMultipleObjects: true,
-    enableClassification: true,
-  });
-
-  const { hasPermission, requestPermission } = useCameraPermission();
-
-  const device = useCameraDevice('back');
-  const format = useCameraFormat(device, Templates.FrameProcessing);
-
-  const fps = Math.min(format?.maxFps ?? 1, targetFps);
-
-  const frameProcessor = useFrameProcessor((frame) => {
-    'worklet';
-
-    runAsync(frame, () => {
-      'worklet';
-      // const text = textRecognizer(frame);
-      // console.log(text);
-
-      // const barcodes = barcodeScanner(frame);
-      // console.log(barcodes);
-
-      // const labels = imageLabeler(frame);
-      // console.log(labels);
-
-      const objects = objectDetector(frame);
-      console.log(objects);
-    });
-  }, []);
-
-  useEffect(() => {
-    !hasPermission && requestPermission();
-  }, [hasPermission, requestPermission]);
-
-  if (!device || !hasPermission) return null;
+  const camera = useRef<RNVCCamera>(null);
 
   return (
-    <View style={styles.container}>
-      <Camera
-        ref={camera}
-        fps={fps}
-        zoom={1}
-        isActive={true}
-        pixelFormat="yuv"
-        device={device}
-        orientation="portrait"
-        format={format}
-        torch={'off'}
-        enableFpsGraph={true}
-        frameProcessor={frameProcessor}
-        resizeMode="cover"
-        style={StyleSheet.absoluteFill}
-        onError={console.error}
-      />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaProvider>
+        <BottomSheetModalProvider>
+          <Camera ref={camera} />
+        </BottomSheetModalProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

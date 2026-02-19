3f9370b1cd1e4274e4fb00154e0bc1067052bbce
Pedro Bueno <pedrolbb.e@gmail.com>
2026-02-19 04:03:57 -0300

    fix(swift): swift code quality fixes

--- ios/Bridge/Plugins/BaseMLKitPlugin.swift
+++ ios/Bridge/Plugins/BaseMLKitPlugin.swift
@@ -1,71 +1,74 @@
 import Foundation
-import VisionCamera
 
-@objc
-public class BaseMLKitPlugin: FrameProcessorPlugin {
+#if canImport(VisionCamera)
+  import VisionCamera
 
-  let proxy: VisionCameraProxyHolder
-  let options: [AnyHashable: Any]?
+  @objc
+  public class BaseMLKitPlugin: FrameProcessorPlugin {
 
-  private let frameProcessInterval: Int
-  private var frameCounter: Int = 0
-  let invertColors: Bool
-  let scaleFactor: CGFloat
+    let proxy: VisionCameraProxyHolder
+    let options: [AnyHashable: Any]?
 
-  var outputOrientation: OutputOrientation?
+    private let frameProcessInterval: Int
+    private var frameCounter: Int = 0
+    let invertColors: Bool
+    let scaleFactor: CGFloat
 
-  public override init(
-    proxy: VisionCameraProxyHolder,
-    options: [AnyHashable: Any]?
-  ) {
-    self.proxy = proxy
-    self.options = options
-    self.frameProcessInterval = options?["frameProcessInterval"] as? Int ?? 0
-    self.invertColors = options?["invertColors"] as? Bool ?? false
+    var outputOrientation: OutputOrientation?
 
-    let sf = options?["scaleFactor"] as? NSNumber
-    let sfValue = sf?.doubleValue ?? 1.0
+    public override init(
+      proxy: VisionCameraProxyHolder,
+      options: [AnyHashable: Any]?
+    ) {
+      self.proxy = proxy
+      self.options = options
+      self.frameProcessInterval = options?["frameProcessInterval"] as? Int ?? 0
+      self.invertColors = options?["invertColors"] as? Bool ?? false
 
-    self.scaleFactor = min(max(CGFloat(sfValue), 0.9), 1.0)
+      let sf = options?["scaleFactor"] as? NSNumber
+      let sfValue = sf?.doubleValue ?? 1.0
 
-    super.init(proxy: proxy, options: options)
-  }
+      self.scaleFactor = min(max(CGFloat(sfValue), 0.9), 1.0)
 
-  public override func callback(
-    _ frame: Frame,
-    withArguments arguments: [AnyHashable: Any]?
-  ) -> Any {
-    // Extract outputOrientation from arguments for rotation support
-    outputOrientation = OutputOrientation(
-      string: arguments?["outputOrientation"] as? String
-    )
+      super.init(proxy: proxy, options: options)
+    }
 
-    // Process 1 frame, skip N frames, repeat
-    // Note: Consider using runAtTargetFps() from Vision Camera instead
-    if frameProcessInterval > 0 {
-      frameCounter += 1
-      if frameCounter <= frameProcessInterval {
+    public override func callback(
+      _ frame: Frame,
+      withArguments arguments: [AnyHashable: Any]?
+    ) -> Any {
+      // Extract outputOrientation from arguments for rotation support
+      outputOrientation = OutputOrientation(
+        string: arguments?["outputOrientation"] as? String
+      )
+
+      // Process 1 frame, skip N frames, repeat
+      // Note: Consider using runAtTargetFps() from Vision Camera instead
+      if frameProcessInterval > 0 {
+        frameCounter += 1
+        if frameCounter <= frameProcessInterval {
+          return [:]
+        }
+        frameCounter = 0
+      }
+
+      do {
+        return try processFrame(frame, withArguments: arguments)
+      } catch {
+        print("[\(pluginName)] Error occurred: \(error.localizedDescription)")
         return [:]
       }
-      frameCounter = 0
     }
 
-    do {
-      return try processFrame(frame, withArguments: arguments)
-    } catch {
-      print("[\(pluginName)] Error occurred: \(error.localizedDescription)")
-      return [:]
+    public func processFrame(
+      _ frame: Frame,
+      withArguments arguments: [AnyHashable: Any]?
+    ) throws -> [String: Any] {
+      fatalError("processFrame must be overridden by subclass")
+    }
+
+    var pluginName: String {
+      return String(describing: type(of: self))
     }
   }
-
-  public func processFrame(
-    _ frame: Frame,
-    withArguments arguments: [AnyHashable: Any]?
-  ) throws -> [String: Any] {
-    fatalError("processFrame must be overridden by subclass")
-  }
-
-  var pluginName: String {
-    return String(describing: type(of: self))
-  }
-}
+#endif
--- ios/Bridge/Plugins/TextRecognitionPlugin.swift
+++ ios/Bridge/Plugins/TextRecognitionPlugin.swift
@@ -1,66 +1,68 @@
 import Foundation
-import VisionCamera
 
 #if MLKIT_TEXT_RECOGNITION
+  #if canImport(VisionCamera)
+    import VisionCamera
 
-  @objc(TextRecognitionPlugin)
-  public class TextRecognitionPlugin: BaseMLKitPlugin {
+    @objc(TextRecognitionPlugin)
+    public class TextRecognitionPlugin: BaseMLKitPlugin {
 
-    private let language: TextRecognitionLanguage
+      private let language: TextRecognitionLanguage
 
-    private lazy var recognitionOptions: TextRecognitionOptions = {
-      TextRecognitionOptions(
-        language: self.language,
-        invertColors: self.invertColors,
-        scaleFactor: self.scaleFactor
-      )
-    }()
+      private lazy var recognitionOptions: TextRecognitionOptions = {
+        TextRecognitionOptions(
+          language: self.language,
+          invertColors: self.invertColors,
+          scaleFactor: self.scaleFactor
+        )
+      }()
 
-    private lazy var recognizeTextUseCase: RecognizeTextUseCase = {
-      let textRecognizer = TextRecognitionServiceFactory.createTextRecognizer(
-        language: self.language
-      )
-      let recognitionService = MLKitTextRecognitionService(
-        textRecognizer: textRecognizer
-      )
-      let imagePreprocessor = ImagePreprocessor()
+      private lazy var recognizeTextUseCase: RecognizeTextUseCase = {
+        let textRecognizer = TextRecognitionServiceFactory.createTextRecognizer(
+          language: self.language
+        )
+        let recognitionService = MLKitTextRecognitionService(
+          textRecognizer: textRecognizer
+        )
+        let imagePreprocessor = ImagePreprocessor()
 
-      return RecognizeTextUseCase(
-        imagePreprocessor: imagePreprocessor,
-        recognitionService: recognitionService
-      )
-    }()
+        return RecognizeTextUseCase(
+          imagePreprocessor: imagePreprocessor,
+          recognitionService: recognitionService
+        )
+      }()
 
-    @objc
-    public override init(
-      proxy: VisionCameraProxyHolder,
-      options: [AnyHashable: Any]? = [:]
-    ) {
-      let languageString = options?["language"] as? String ?? "LATIN"
-      self.language =
-        TextRecognitionLanguage(rawValue: languageString) ?? .latin
+      @objc
+      public override init(
+        proxy: VisionCameraProxyHolder,
+        options: [AnyHashable: Any]? = [:]
+      ) {
+        let languageString = options?["language"] as? String ?? "LATIN"
+        self.language =
+          TextRecognitionLanguage(rawValue: languageString) ?? .latin
 
-      super.init(proxy: proxy, options: options)
+        super.init(proxy: proxy, options: options)
+      }
+
+      public override func processFrame(
+        _ frame: Frame,
+        withArguments arguments: [AnyHashable: Any]?
+      ) throws -> [String: Any] {
+        let options = TextRecognitionOptions(
+          language: recognitionOptions.language,
+          invertColors: recognitionOptions.invertColors,
+          outputOrientation: outputOrientation,
+          scaleFactor: recognitionOptions.scaleFactor
+        )
+
+        let result = try recognizeTextUseCase.execute(
+          frame: frame,
+          options: options
+        )
+
+        return TextRecognitionSerializer.toReactNativeMap(result)
+      }
     }
 
-    public override func processFrame(
-      _ frame: Frame,
-      withArguments arguments: [AnyHashable: Any]?
-    ) throws -> [String: Any] {
-      let options = TextRecognitionOptions(
-        language: recognitionOptions.language,
-        invertColors: recognitionOptions.invertColors,
-        outputOrientation: outputOrientation,
-        scaleFactor: recognitionOptions.scaleFactor
-      )
-
-      let result = try recognizeTextUseCase.execute(
-        frame: frame,
-        options: options
-      )
-
-      return TextRecognitionSerializer.toReactNativeMap(result)
-    }
-  }
-
+  #endif
 #endif
